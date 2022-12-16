e3c203a8cfd352c9b118512666900aadd9f21e34
Megan <esler6724@gmail.com>
2022-12-15 17:07:00 -0800

    commit tree 0df2ea0267a7f717fabe28f373668d3e5636b157

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(res => res.json())
+.then(object => {
+    for(let element of object) {
+        document.createElement(element)
+    }
+})
